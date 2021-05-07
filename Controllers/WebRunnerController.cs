bab9fcaf3b8475e1140ba4b213415cf536486a04
reallyalexpro <reallyalexpro@yandex.ru>
2021-05-07 23:13:53 +0300

    Добавьте файлы проекта.

---
+++ Controllers/WebRunnerController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace WebRunner.Controllers
+{
+    [ApiController]    
+    public class WebRunnerController : ControllerBase
+    {
+        protected ILogger logger;
+        private IConfiguration _config;
+
+        public WebRunnerController(IConfiguration config)
+        {            
+            _config = config;
+        }
+
+        [HttpGet]
+        [Route("run/{name}")]
+        public IActionResult Index(string name)
+        {
+            logger = LogManager.GetLogger(name);
+
+            var commands = _config.GetSection("Commands").GetChildren();
+
+            var command = commands.Where(item => item["name"] == name).FirstOrDefault();
+
+            if (command == null) return NotFound("Command not found");
+            
+            var process = new Process();
+            process.StartInfo.FileName = command["command"];
+            process.StartInfo.Arguments = command["arguments"];
+            process.StartInfo.WorkingDirectory = @"c:\";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.OutputDataReceived += (sender, a) => logger.Info(a.Data);
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+
+            return Ok("Done");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebRunner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureLogging((context, logging) => {
+                    logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+
+                    if (context.HostingEnvironment.IsDevelopment())
+                    {
+                        logging.AddConsole();
+                        logging.AddDebug();
+                    }
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    string basePath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+                    config.SetBasePath(basePath).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
